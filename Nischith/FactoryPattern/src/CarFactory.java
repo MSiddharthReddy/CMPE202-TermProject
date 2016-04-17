7d4ca43cfe51066c5081aa02dbfbce11ea930b70
nischith-bn <nischithbedachuvaly.nataraja@sjsu.edu>
2016-04-16 23:29:04 -0700

    Implemented Sample Factory Pattern #20

--- Nischith/FactoryPattern/src/CarFactory.java
+++ Nischith/FactoryPattern/src/CarFactory.java
@@ -6,14 +6,17 @@ public class CarFactory {
 		if(carType=="Big")
 		{
 			car= new BigCar();
+			System.out.println("Big car");
 		}
 		else if(carType=="Medium")
 		{
 			car = new MediumCar();
+			System.out.println("Small car");
 		}
 		else if(carType=="Small")
 		{
 			car = new SmallCar();
+			System.out.println("Medium car");
 		}
 		return car;
 	}
--- Nischith/FactoryPattern/src/Package.java
+++ Nischith/FactoryPattern/src/Package.java
@@ -1,4 +1,4 @@
 
 public interface Package {
-	public Package createPackage();
+	
 }
